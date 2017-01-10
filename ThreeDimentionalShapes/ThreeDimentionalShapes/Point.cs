8f9c41ffbd0ea02832207335d8f593456a25601e
sahak <sahak@DESKTOP-6AC4FLU>
2017-01-10 18:12:12 +0400

    Start program

---
+++ ThreeDimentionalShapes/ThreeDimentionalShapes/Point.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreeDimentionalShapes
+{
+    public struct Point
+    {
+        public float x { get; set; }
+        public float y { get; set; }
+        public float z { get; set; }
+
+        public Point(float x,float y,float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+    }
+}
